02e367a5abab5b206dae3896becd8f03e18a89f1
eyakub <eyakubsorkar@gmail.com>
2020-04-12 07:07:49 +0600

    Updates

---
+++ precache-manifest.7e805dc2323ffe89b0d264c9e588cb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75734aa303435c3b27fcfd1e3fec8231",
+    "url": "/stack-todos-reactjs/index.html"
+  },
+  {
+    "revision": "1966464376cf9a7be31f",
+    "url": "/stack-todos-reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7357d24202a1d21aa21f",
+    "url": "/stack-todos-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1966464376cf9a7be31f",
+    "url": "/stack-todos-reactjs/static/js/2.3db503e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stack-todos-reactjs/static/js/2.3db503e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7357d24202a1d21aa21f",
+    "url": "/stack-todos-reactjs/static/js/main.26242d0e.chunk.js"
+  },
+  {
+    "revision": "a587daf25ece9872a922",
+    "url": "/stack-todos-reactjs/static/js/runtime-main.a1588194.js"
+  }
+]);
